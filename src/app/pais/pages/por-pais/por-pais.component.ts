bbb7312ad96b4f546f34f9a9b268386db7fb9633
David <david.lazaro@babelgroup.com>
2022-04-21 11:32:44 +0200

    Fin del proyecto paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,9 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li { cursor: pointer;}
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,12 +16,15 @@ export class PorPaisComponent {
  termino:string = '';
  hayError:boolean = false;
  paises: Country[] = [];
+ paisesSugeridos: Country[] = [];
+ mostrarSugerencias: boolean = false;
 
    constructor(private paisService: PaisService) { }
 
    buscar( termino:string) {
      this.hayError = false;
      this.termino = termino;
+     this.mostrarSugerencias = false;
      // como hemos puesto en el servicio que retorne un observable
      // aquí nos subscribimos para recibir la respuesta
      this.paisService.buscarPais(this.termino)
@@ -33,8 +39,18 @@ export class PorPaisComponent {
 
    sugerencias(termino:string) {
      this.hayError = false;
-     // TODO: crear sugerencias
+     this.termino = termino;
+     this.mostrarSugerencias = true;
 
+     this.paisService.buscarPais(termino)
+     .subscribe(paises => this.paisesSugeridos = paises.splice(0,5),
+     (err) => this.paisesSugeridos = []
+     );
+   }
+
+   buscarSugerido(termino: string)
+   {
+     this.buscar(termino);
    }
 
 }
